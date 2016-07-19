b827b32e39a5a17072d30f81ef5fd7c14c2c12ef
Iván Corrales <developer@wesovi.com>
2016-07-19 14:39:58 +0200

    feat: adding code to make v2 work as expected

--- README.md
+++ README.md
@@ -21,4 +21,4 @@ If [available in Hex](https://hex.pm/docs/publish), the package can be installed
       [applications: [:elixir_maru_training]]
     end
     ```
-
+##
--- lib/elixir_maru_training/router/track_router_v2.ex
+++ lib/elixir_maru_training/router/track_router_v2.ex
@@ -1,9 +1,22 @@
 defmodule ElixirMaruTraining.TrackRouterV2 do
   use Maru.Router
+  import Ecto.Query
   require Logger
   alias ElixirEctoTraining.Track, warn: true
   alias ElixirEctoTraining.Repo, warn: true
 
+
+  def allTracks do
+    query = from track in ElixirEctoTraining.Track,
+        select: (
+            %{ trackId: track.id, title: track.title, singer: track.singer}
+        )
+    query |> ElixirEctoTraining.Repo.all
+  end
+
+  def inserTrack(track) do
+    ElixirEctoTraining.Repo.insert(track)
+  end
 
   namespace :tracks do
 
@@ -11,18 +24,32 @@ defmodule ElixirMaruTraining.TrackRouterV2 do
 
         @desc "Return the list of tracks in the database"
         get do
-            tracks_as_json = Repo.Track.all
+            tracks_as_json = allTracks()
             |> List.wrap
             |> Poison.encode!
-
             Logger.debug "Tracks #{inspect tracks_as_json}"
             conn
             |> put_status(200)
             |> json(tracks_as_json)
-
         end
 
-    end
+
+
+         @desc "Add a new track and return a 201 http status code"
+         params do
+            requires :title, type: String
+            requires :singer, type: String
+            optional :score, type: Atom, values: [:bad, :normal, :good, :awesome], default: :normal
+         end
+         post do
+            track = %Track{title: params[:title], singer: params[:singer]}
+            inserTrack(track)
+            conn
+            |> put_status(204)
+            |> json(%{})
+         end
+     end
+
   end
 
 end
